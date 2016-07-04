292defb14e3ab8c24c0c10dd9954d6cfae0d8f78
Sirajudheen K <sirajudheen@mobomo-imac.local>
2016-07-04 12:38:59 +0530

    Initial Commit

---
+++ SpringView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpringView
+//
+//  Created by Sirajudheen K on 04/07/16.
+//  Copyright © 2016 Sirajudheen K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
